a3a0810cc312fecea2d19bf23c4d2071cb98ed7d
Keep Simple <keepsimple17@gmail.com>
2017-11-03 18:02:40 -0500

    Import styles inside CSS instead of JS [f:30m]
    Also use smaller rem for small screens.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'bootstrap/dist/css/bootstrap-reboot.css'
-import 'griddity/griddity.css'
 import './styles/index.css'
 
 import App from './components/App/App'
