5e788ef092b306418bc2b5946372f1c93eb43690
Keep Simple <keepsimple17@gmail.com>
2017-11-03 11:00:02 -0500

    Add Bootstrap's reboot and griddity [f:10m]

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import 'bootstrap/dist/css/bootstrap-reboot.css'
+import 'griddity/griddity.css'
 import './styles/index.css'
+
 import App from './components/App/App'
 import registerServiceWorker from './registerServiceWorker'
 
