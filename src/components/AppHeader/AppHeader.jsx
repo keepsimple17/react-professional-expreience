2026784418ef7bda364c4c07b6c2dc116c7070c1
Keep Simple <keepsimple17@gmail.com>
2017-11-03 18:04:15 -0500

    Complete header [f:30m]

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -2,15 +2,32 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import logo from '../../img/logo-blue.svg'
+import appleStoreBadge from '../../img/app-store-badge.svg'
 
 import './AppHeader.css'
 
 const AppHeader = () => (
-  <header className="app-header">
+  <header className="app-header text-center text-md-left">
     <div className="container">
-      <Link to="/">
-        <img src={logo} className="logo" alt="CrisisEarth logo" />
-      </Link>
+      <div className="row justify-content-between align-items-center">
+        <div className="col-12 col-md-auto">
+          <Link to="/">
+            <img src={logo} className="logo" alt="CrisisEarth logo" />
+          </Link>
+        </div>
+        <div className="col-12 col-md-auto d-none d-md-flex">
+          <p className="offer">
+            Track and offset your carbon footprint
+            <a href="https://store.apple.com">
+              <img
+                src={appleStoreBadge}
+                className="download-badge"
+                alt="Download on apple store"
+              />
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   </header>
 )
