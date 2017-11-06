9ccdd53135d0cc9323df7139c3878fc69d479aed
Keep Simple <keepsimple17@gmail.com>
2017-11-06 14:28:08 -0500

    Render HomePage on proper route [f:10m]

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import AppFooter from '../AppFooter/AppFooter'
 import AppHeader from '../AppHeader/AppHeader'
+import HomePage from '../HomePage/HomePage'
 
 const App = () => (
   <Router>
     <div>
       <AppHeader />
-      <Route exact path="/" render={() => 'Home'} />
+      <Route exact path="/" component={HomePage} />
       <Route
         exact
         path="/profile/:username"
