bcc3de73614482b9197321c3ce497c974fb46f6d
Keep Simple <keepsimple17@gmail.com>
2017-11-08 14:15:11 -0500

    Add Top Producers and their trips to HomePage [f:2h]

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,36 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 
+import api from '../../api'
 import SingleInputForm from '../SingleInputForm/SingleInputForm'
+import ProfileCard from '../ProfileCard/ProfileCard'
+import SmallTripBox from '../SmallTripBox/SmallTripBox'
 import searchIcon from '../../img/icons/search.svg'
 
 import './HomePage.css'
 
-const HomePage = ({ history }) => (
-  <section className="home-page">
-    <section className="search-hero">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h2 className="title text-center">
-              Enter an Instagram username to begin:
-            </h2>
+class HomePage extends Component {
+  constructor () {
+    super()
+    this.state = {
+      topProducers: []
+    }
+  }
+
+  async componentWillMount () {
+    const topProducers = await api.fetchTopProducers()
+
+    this.setState(() => ({
+      topProducers: topProducers || []
+    }))
+  }
+
+  render () {
+    return (
+      <section className="home-page">
+        <section className="search-hero">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h2 className="title text-center">
+                  Enter an Instagram username to begin:
+                </h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <SingleInputForm
+                  iconUrl={searchIcon}
+                  placeholder="asimon9633"
+                  onSubmit={(event, username) => this.props.history.push(`/profile/${username}`)}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <SingleInputForm
-              iconUrl={searchIcon}
-              placeholder="asimon9633"
-              onSubmit={(event, username) => history.push(`/profile/${username}`)}
-            />
+        </section>
+        <section className="top-producers">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h2 className="top-producers-heading">
+                  Worst carbon producers who are not offsetting
+                </h2>
+              </div>
+            </div>
+            {this.state.topProducers.map(profile => (
+              <div className="row producer-row" key={profile.username}>
+                <div className="col col-lg-5">
+                  <ProfileCard profile={profile} />
+                </div>
+                <div className="d-none d-lg-block col-lg-7">
+                  <ul className="trip-list">
+                    {profile.trips.map(trip => (
+                      <li className="trip-item">
+                        <SmallTripBox trip={trip} key={trip.pictureUrl} />
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
           </div>
-        </div>
-      </div>
-    </section>
-  </section>
-)
+        </section>
+      </section>
+    )
+  }
+}
 
 HomePage.propTypes = {
   history: PropTypes.shape({
