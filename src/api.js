6ba2a2eaf34472e8b5679eba7ff62b872a88feed
Keep Simple <keepsimple17@gmail.com>
2017-11-08 14:11:07 -0500

    Add dummy API support to fetch top producers [f:3h]
    Using randomuser.me and unplash.com.

---
+++ src/api.js
@@ -0,0 +1,31 @@
+/* global fetch */
+
+const { REACT_APP_API_URI } = process.env
+
+const getRandomInt = (min = 0, max = 1) => {
+  const floor = Math.ceil(min)
+  const ceil = Math.floor(max)
+  return Math.floor(Math.random() * (ceil - floor)) + floor
+}
+
+const getRandomTrip = () => ({
+  carbonOutput: getRandomInt(100, 1000),
+  pictureUrl: `https://source.unsplash.com/random/420x420?${String.fromCharCode(getRandomInt(65, 91))}`
+})
+
+const formatProfile = profile => ({
+  carbonOutput: getRandomInt(1000, 10000),
+  fullName: `${profile.name.first || ''} ${profile.name.last || ''}`.trim(),
+  profilePictureUrl: profile.picture.large,
+  trips: Array.from({ length: getRandomInt(3, 6) }, getRandomTrip),
+  username: profile.login.username
+})
+
+const fetchTopProducers = (n = 8) => fetch(`${REACT_APP_API_URI}?results=${n}`)
+  .then(res => res.json())
+  .then(data => data.results)
+  .then(profiles => profiles.map(formatProfile))
+
+export default {
+  fetchTopProducers
+}
