932c946e16087ab18702289a37bd2de0e461bf14
Keep Simple <keepsimple17@gmail.com>
2017-11-21 09:10:43 -0500

    Add loading message to friends list [f:1h]

--- src/components/ProfilePage/FriendsList.jsx
+++ src/components/ProfilePage/FriendsList.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import cn from '../../util/cn'
 import ProfileCard from '../ProfileCard/ProfileCard'
 
+import './FriendsList.css'
+
 const FriendsList = ({ data }) => (
-  <ul className="friend-list">
+  <ul
+    {...cn(
+      'friends-list',
+      {
+        '-loading': data.loading,
+        '-empty': !data.friends || !data.friends.lenght
+      }
+    )}
+  >
     {data.friends &&
       data.friends.map((profile, i) => (
         <li className="friend-item" key={profile.username}>
           <ProfileCard profile={profile} />
         </li>
       ))}
+    <li className="loading-message">
+      Loading friends...
+    </li>
   </ul>
 )
 
