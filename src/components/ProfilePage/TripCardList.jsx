78cf694482f913487757c98f61a99133f1fbbc16
Keep Simple <keepsimple17@gmail.com>
2017-11-21 09:26:24 -0500

    Add loading state to TripCardList [f:1h]

--- src/components/ProfilePage/TripCardList.jsx
+++ src/components/ProfilePage/TripCardList.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import cn from '../../util/cn'
 import TripCard from '../TripCard/TripCard'
 
+import './TripCardList.css'
+
 const TripCardList = ({ data }) => (
-  <div className="row">
+  <div
+    {...cn(
+      'row',
+      'trip-card-list',
+      {
+        '-loading': !!data.loading,
+        '-empty': !data.trips || !data.trips.length
+      }
+    )}
+  >
     {data.trips &&
       data.trips.map(trip => (
         <div className="col-12 col-md-6 col-xl-4 trip-col" key={trip.pictureUrl}>
           <TripCard profile={data.profile} trip={trip} />
         </div>
       ))}
+    {!!data.loading && (
+      <div className="col-12 loading-message">
+        <p>
+          Loading travel posts...
+        </p>
+      </div>
+    )}
   </div>
 )
 
