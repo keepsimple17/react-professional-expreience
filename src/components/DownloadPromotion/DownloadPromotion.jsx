f30475ef044dcadc26df4802d343f20a62193134
Keep Simple <keepsimple17@gmail.com>
2017-11-22 14:17:19 -0500

    Place DownloadPromotion into its own component [f:30m]

---
+++ src/components/DownloadPromotion/DownloadPromotion.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './DownloadPromotion.css'
+
+import appleStoreBadge from '../../img/app-store-badge.svg'
+
+const DownloadPromotion = () => (
+  <div className="download-promotion">
+    <div className="container">
+      <div className="row">
+        <div className="col col-md-6 offset-md-6">
+          <h3 className="download-heading">How do you offset your carbon?</h3>
+          <p className="download-paragraph">
+            When you download our app, it will ask you for your home address, average monthly
+            energy bill, and begin monitoring your transportation by car, subway, or flight
+            through your smartphone.
+          </p>
+          <p className="download-paragraph">
+            As you generate carbon, we will automatically bill your credit card up to an amount
+            you specify each month.
+          </p>
+          <p>
+            <a className="link" href="https://store.apple.com">
+              <img
+                src={appleStoreBadge}
+                className="download-badge"
+                alt="Download on apple store"
+              />
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default DownloadPromotion
--- src/components/ProfilePage/PrivateProfile.jsx
+++ src/components/ProfilePage/PrivateProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import getName from '../../util/getName'
-import appleStoreBadge from '../../img/app-store-badge.svg'
+import DownloadPromotion from '../DownloadPromotion/DownloadPromotion'
 
 import './PrivateProfile.css'
 
@@ -35,33 +35,7 @@ const PrivateProfile = ({ profile }) => (
         </div>
       </div>
     </div>
-    <div className="download-promotion">
-      <div className="container">
-        <div className="row">
-          <div className="col col-md-6 offset-md-6">
-            <h3 className="download-heading">How do you offset your carbon?</h3>
-            <p className="download-paragraph">
-              When you download our app, it will ask you for your home address, average monthly
-              energy bill, and begin monitoring your transportation by car, subway, or flight
-              through your smartphone.
-            </p>
-            <p className="download-paragraph">
-              As you generate carbon, we will automatically bill your credit card up to an amount
-              you specify each month.
-            </p>
-            <p>
-              <a className="link" href="https://store.apple.com">
-                <img
-                  src={appleStoreBadge}
-                  className="download-badge"
-                  alt="Download on apple store"
-                />
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <DownloadPromotion />
   </div>
 )
 
