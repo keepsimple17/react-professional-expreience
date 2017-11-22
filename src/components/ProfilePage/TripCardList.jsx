9fc4be844f7b237d8b3f1379c1ac2a62d048b935
Keep Simple <keepsimple17@gmail.com>
2017-11-22 13:55:06 -0500

    Fix alignment of TripCardList [f:30m]

--- src/components/ProfilePage/TripCardList.jsx
+++ src/components/ProfilePage/TripCardList.jsx
@@ -8,28 +8,26 @@ import './TripCardList.css'
 
 const TripCardList = ({ data }) => (
   <div
-    {...cn(
-      'row',
-      'trip-card-list',
-      {
-        '-loading': !!data.loading,
-        '-empty': !data.trips || !data.trips.length
-      }
-    )}
+    {...cn('trip-card-list', {
+      '-loading': !!data.loading,
+      '-empty': !data.trips || !data.trips.length
+    })}
   >
-    {data.trips &&
-      data.trips.map(trip => (
-        <div className="col-12 col-md-6 col-xl-4 trip-col" key={trip.pictureUrl}>
-          <TripCard profile={data.profile} trip={trip} />
+    <div className="row">
+      {data.trips &&
+        data.trips.map(trip => (
+          <div className="col-12 col-md-6 col-xl-4 trip-col" key={trip.pictureUrl}>
+            <TripCard profile={data.profile} trip={trip} />
+          </div>
+        ))}
+      {!!data.loading && (
+        <div className="col-12 loading-message">
+          <p>
+            Loading travel posts...
+          </p>
         </div>
-      ))}
-    {!!data.loading && (
-      <div className="col-12 loading-message">
-        <p>
-          Loading travel posts...
-        </p>
-      </div>
-    )}
+      )}
+    </div>
   </div>
 )
 
