880a53be03a2faff54805c72db3708a93efa2d9b
Keep Simple <keepsimple17@gmail.com>
2017-12-26 17:30:29 +0100

    Make ProfileCard an observer [f:20m]

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { injectIntl, FormattedNumber } from 'react-intl'
+import { observer } from 'mobx-react'
 
 import { gramsToPounds } from '../../util/conversions'
 
@@ -62,4 +63,4 @@ ProfileCard.propTypes = {
   }).isRequired
 }
 
-export default injectIntl(ProfileCard)
+export default injectIntl(observer(ProfileCard))
