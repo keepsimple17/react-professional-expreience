6489324deabd7dfa4bee234fe0342d5a14a9fc8d
Keep Simple <keepsimple17@gmail.com>
2018-01-12 01:01:18 +0100

    Fix trips collapsing in small screens [f:1h]

--- src/components/TripsTimeline/TripsTimeline.jsx
+++ src/components/TripsTimeline/TripsTimeline.jsx
@@ -9,29 +9,25 @@ import './TripsTimeline.css'
 
 const TripsTimeline = ({ trips }) => (
   <div className="trips-timeline">
-    <div className="row">
-      <ul className="trip-list">
-        {trips.trips &&
-          trips.trips.map((trip, i, l, last = l.length === i + 1) => (
-            <li className="trip-item" key={trip.pictureUrl}>
-              <div className="row">
-                <div className="col">
-                  <DestinationItem trip={trip} />
-                </div>
-                {!last && (
-                  <div className="col-auto">
-                    <DistanceItem trip={trip} />
-                  </div>
-                )}
+    <ul className="trip-list">
+      {trips.trips &&
+        trips.trips.map((trip, i, l, last = l.length === i + 1) => (
+          <li className="trip-item" key={trip.pictureUrl}>
+            <div className="row flex-nowrap">
+              <div className="col">
+                <DestinationItem trip={trip} />
               </div>
-            </li>
-          ))}
-      </ul>
-    </div>
-    <div className="row">
-      <div className="controls">
-        controls
-      </div>
+              {!last && (
+                <div className="col-auto">
+                  <DistanceItem trip={trip} />
+                </div>
+              )}
+            </div>
+          </li>
+        ))}
+    </ul>
+    <div className="controls">
+      controls
     </div>
   </div>
 )
