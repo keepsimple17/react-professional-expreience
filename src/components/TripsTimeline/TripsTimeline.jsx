cb6b4b3290971286323ce4661bb7edfde932aa68
Keep Simple <keepsimple17@gmail.com>
2018-01-17 15:58:44 +0100

    Limit TripsTimeline to 5 by default [f:20m]

--- src/components/TripsTimeline/TripsTimeline.jsx
+++ src/components/TripsTimeline/TripsTimeline.jsx
@@ -8,11 +8,11 @@ import DistanceItem from './DistanceItem'
 
 import './TripsTimeline.css'
 
-const TripsTimeline = ({ trips }) => (
+const TripsTimeline = ({ limit, trips }) => (
   <div className="trips-timeline">
     <ul className="trip-list">
       {trips.trips &&
-        trips.trips.map((trip, i, l, last = l.length === i + 1) => (
+        trips.trips.slice(0, limit).map((trip, i, l, last = l.length === i + 1) => (
           <li className="trip-item" key={trip.pictureUrl}>
             <div className="row flex-nowrap align-items-center">
               <div className="col">
@@ -32,7 +32,7 @@ const TripsTimeline = ({ trips }) => (
         <div className="row">
           <div className="col-12">
             <ul className="bullet-controls">
-              {trips.trips.map((trip, i) => (
+              {trips.trips.slice(0, limit).map((trip, i) => (
                 <li className="bullet-item" key={trip.pictureUrl}>
                   <span
                     {...cn('bullet', {
@@ -51,7 +51,12 @@ const TripsTimeline = ({ trips }) => (
   </div>
 )
 
+TripsTimeline.defaultProps = {
+  limit: 5
+}
+
 TripsTimeline.propTypes = {
+  limit: PropTypes.number,
   trips: PropTypes.shape({
     trips: MobxPropTypes.observableArrayOf(PropTypes.shape({
       pictureUrl: PropTypes.string
